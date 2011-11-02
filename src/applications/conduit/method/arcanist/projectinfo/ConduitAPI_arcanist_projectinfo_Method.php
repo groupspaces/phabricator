49da5e8335599db50b2d53024980ca23385d6b12
Dave Ingram <dave@dmi.me.uk>
2011-11-02 13:51:11 +0000

    Bugfix: if repository is not available, we cannot get its encoding

--- src/applications/conduit/method/arcanist/projectinfo/ConduitAPI_arcanist_projectinfo_Method.php
+++ src/applications/conduit/method/arcanist/projectinfo/ConduitAPI_arcanist_projectinfo_Method.php
@@ -67,7 +67,7 @@ class ConduitAPI_arcanist_projectinfo_Method
       'phid'            => $project->getPHID(),
       'repositoryPHID'  => $repository_phid,
       'tracked'         => $tracked,
-      'encoding'        => $repository->getDetail('encoding')
+      'encoding'        => $repository ? $repository->getDetail('encoding') : null
     );
   }
 
